1bda4473b016ccdd16a9f1e49b32bf365f09edb7
Mark Gregg <mrkgregg@gmail.com>
2016-07-30 22:33:52 +0100

    tested MIXIN

---
+++ test/testMixin.js
@@ -0,0 +1,29 @@
+describe("To test Mixin", function () {
+  var baseClass;
+  var subClass;
+
+	beforeEach(function () {
+    baseClass = {
+      shouldBeAdded : function(){return "Should be added";},
+      shouldBeOverride :  function(){return "Should be overriden";}
+    };
+
+    subClass = {
+      shouldBeOverride:  function(){return "This has been overriden";}
+    };
+
+	});
+
+
+	it("should add properties of baseClass to subClass that aren't already defined", function() {
+			MIXIN(baseClass, subClass);
+      expect(subClass.shouldBeAdded).toBeDefined();
+	});
+
+  it("should override properties defined in both classes, using the subclass version", function() {
+      MIXIN(baseClass, subClass);
+      expect(subClass.shouldBeOverride()).toEqual("This has been overriden");
+  });
+
+
+});
